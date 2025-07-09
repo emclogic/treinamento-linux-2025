3e2ab14d7d93b12a17e4165f1ba10b2179a877e2
Fernando Luiz Cola <fernando.cola@emc-logic.com>
2025-07-09 15:14:10 -0300

    fix typos

--- i2c-oled/status.py
+++ i2c-oled/status.py
@@ -62,13 +62,13 @@ while True:
 
     # Shell scripts for system monitoring from here:
     # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d' ' -f1"
+    cmd = "hostname | cut -d' ' -f1"
     IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = 'cut -f 1 -d " " /proc/loadavg'
     CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    cmd = "free -m | awk 'NR==2{printf "Mem: %s/%s MB  %.2f%%\n", $3,$2,$3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+    cmd = 'df -h | awk '$NF=="/"{printf "Disk: %s/%s used  (%s)\n", $3,$2,$5}''
     Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
     # Write four lines of text.
